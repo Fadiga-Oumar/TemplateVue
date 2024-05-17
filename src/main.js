89e115b5ed44ae5c979c07862949cac0ad4a6fad
Oumar-Fadiga <Fadigaalphaoumar777@gmail.com>
2024-05-17 11:04:21 +0100

    le depot du template

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// main.js
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import ArgonDashboard from "./argon-dashboard";
+
+const appInstance = createApp(App);
+
+// Initialiser l'état Vuex depuis le stockage local
+store.dispatch('initFromLocalStorage');
+
+appInstance.use(store);
+appInstance.use(router);
+appInstance.use(ArgonDashboard);
+
+appInstance.mount("#app");
